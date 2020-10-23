31e9c5ff017cbc7264a81efdb3007ca9a557360b
Reginaldo Morikawa <morikawa77@gmail.com>
2020-10-23 02:17:51 -0300

    Challenge solved and added a README

---
+++ README.md
@@ -0,0 +1,38 @@
+<img alt="GoStack" src=".github/header-desafios-new.png" style="width: 100%;"/>
+
+<h3 align="center">
+  Challenge 03: ReactJS Concepts
+</h3>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=15C3D6&labelColor=000000">
+</p>
+
+### 🚀 About the challenge
+
+In this challenge, an application was created to practice what I learned in ReactJS.
+
+The application has the purpose of storing repositories, which will allow you to list, update and delete repositories.
+
+### Application Features
+
+List the repositories for your API: You should be able to create a list with the title field of all the repositories that are registered in your API.
+
+Adding a repository to your API: You should be able to add a new item to your API using a button with the text Add and, after creation, you should be able to display its name after registration.
+
+Remove a repository from your API: For each item on your list, you must have a button with the text Remove which, when clicked, will call a function to remove that item from the list of your frontend and your API.
+
+
+### Tests Specification
+
+- **`should be able to add new repository`**: For this test to pass, your application must allow a repository to be added to your backend and listed on your frontend within an LI.
+
+- **`should be able to remove repository`**: In order for this test to pass, your application must allow the item to be removed from the list by clicking on the remove button that will be inside the LI of the added repository.
+
+### Layout adjustments
+<img alt="GoStack" src=".github/header-desafios-new.png" style="width: 100%;"/>
+I changed some layout items to better display the content and improve UX.
+
+---
+
+Made with ❤️ by morikawa77
--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repositório ${Date.now()}`,
+      url: "https://github.com/morikawa77",
+      techs: [
+        "PHP",
+        "html",
+        "javascript"
+      ],
+      likes: 0
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <a target="_blank" rel="noopener noreferrer" href={repository.url}>{repository.title}</a><br />
+            Techs: {repository.techs.join(", ")}<br />
+            Likes: {repository.likes}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        )
+        )}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button width={350} onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
